0143ac7ed3607d56150779376a210632b9d6636f
Kasper <kasper.allais@gmail.com>
2025-03-03 15:37:41 -0700

    added vimtex

--- nvim/lua/plugins/plugins.lua
+++ nvim/lua/plugins/plugins.lua
@@ -2,5 +2,12 @@ return {
 	"williamboman/mason.nvim",
 	"williamboman/mason-lspconfig.nvim",
 	"neovim/nvim-lspconfig",
-	"jiangmiao/auto-pairs"
+	"jiangmiao/auto-pairs",
+	{
+		"lervag/vimtex",
+		lazy = false,
+		init = function()
+			vim.g.vimtex_view_method = "zathura"
+		end
+}
 }
